8a1b546b6176888cfbffcbafd8e28a838db50b12
Bruno Basto <bruno.basto@liferay.com>
2019-05-02 19:34:20 -0300

    LPS-93027 implements custom pagination item renderer for the Success Page use case

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
@@ -603,7 +603,7 @@ class LayoutProvider extends Component {
 	}
 
 	_handlePaginationNextClicked() {
-		const {activePage, pages} = this;
+		const {activePage, pages} = this.state;
 
 		handlePaginationNextClicked(
 			{
@@ -615,7 +615,7 @@ class LayoutProvider extends Component {
 	}
 
 	_handlePaginationPreviousClicked() {
-		const {activePage} = this;
+		const {activePage} = this.state;
 
 		handlePaginationPreviousClicked({activePage}, this.dispatch.bind(this));
 	}
