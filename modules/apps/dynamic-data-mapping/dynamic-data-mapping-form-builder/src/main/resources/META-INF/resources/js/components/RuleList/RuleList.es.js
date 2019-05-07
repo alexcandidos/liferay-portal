2e8f99274e45cefc1447ca00125c4aa3662b0c27
Bruno Basto <bruno.basto@liferay.com>
2019-05-07 10:40:50 -0300

    LPS-93027 Fix formatting of Jump To Page label

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleList/RuleList.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleList/RuleList.es.js
@@ -32,7 +32,6 @@ class RuleList extends Component {
 	}
 
 	prepareStateForRender(states) {
-		const {pages} = this;
 		const rules = this._setDataProviderNames(states);
 
 		return {
@@ -52,7 +51,7 @@ class RuleList extends Component {
 						...rule,
 						actions: rule.actions.map(
 							action => {
-								let newAction = '';
+								let newAction;
 
 								if (action.action === 'auto-fill') {
 									const {inputs, outputs} = action;
@@ -66,40 +65,16 @@ class RuleList extends Component {
 										outputLabel
 									};
 								}
-
-								let fieldLabel = this._getFieldLabel(action.target);
-
-								if (action.action == 'jump-to-page') {
-									const fieldTarget = (parseInt(action.target, 10) + 1).toString();
-
-									if (action.label) {
-										fieldLabel = action.label;
-									}
-									else {
-										const maxPageIndexRes = maxPageIndex(rule.conditions, pages);
-
-										const pageOptionsList = pageOptions(pages, maxPageIndexRes);
-
-										const selectedPage = pageOptionsList.find(
-											option => {
-												return option.value == fieldTarget;
-											}
-										);
-
-										fieldLabel = selectedPage.label;
-									}
-
+								else if (action.action == 'jump-to-page') {
 									newAction = {
 										...action,
-										label: fieldLabel,
-										target: fieldTarget
+										label: this._getJumpToPageLabel(rule, action)
 									};
 								}
 								else {
 									newAction = {
 										...action,
-										label: fieldLabel,
-										target: fieldLabel
+										label: this._getFieldLabel(action.target)
 									};
 								}
 
@@ -163,6 +138,26 @@ class RuleList extends Component {
 		const pages = this.pages;
 
 		return getFieldProperty(pages, fieldName, 'type');
+	}
+
+	_getJumpToPageLabel(rule, action) {
+		const {pages} = this;
+		let pageLabel = '';
+
+		const fieldTarget = (parseInt(action.target, 10) + 1).toString();
+		const maxPageIndexRes = maxPageIndex(rule.conditions, pages);
+		const pageOptionsList = pageOptions(pages, maxPageIndexRes);
+		const selectedPage = pageOptionsList.find(
+			option => {
+				return option.value == fieldTarget;
+			}
+		);
+
+		if (selectedPage) {
+			pageLabel = selectedPage.label;
+		}
+
+		return pageLabel;
 	}
 
 	_getOperandLabel(operands, index) {
