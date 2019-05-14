02b1b2f402794f71c7715875a215e05e7e2acb9a
Bruno Basto <bruno.basto@liferay.com>
2019-05-13 23:40:29 -0300

    LPS-93027 Show pagination on success page

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/FormBuilder/withMultiplePages.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/FormBuilder/withMultiplePages.es.js
@@ -25,7 +25,14 @@ const withMultiplePages = ChildComponent => {
 				];
 			}
 
-			return pages;
+			return pages.map(
+				page => {
+					return {
+						...page,
+						enabled: true
+					};
+				}
+			);
 		}
 
 		getPaginationPosition() {
