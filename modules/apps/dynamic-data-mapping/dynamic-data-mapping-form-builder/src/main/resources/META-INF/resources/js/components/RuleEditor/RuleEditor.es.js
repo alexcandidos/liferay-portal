63000a7364d05eefe5b33c4c36ab4fd147bde8d1
Bruno Basto <bruno.basto@liferay.com>
2019-05-15 18:48:18 -0300

    WIP

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
@@ -1699,7 +1699,12 @@ class RuleEditor extends Component {
 				) {
 					action.target = '';
 				}
-				else if (action.action == 'auto-fill') {
+				else if (
+					rule &&
+					rule.actions &&
+					rule.actions[index] &&
+					action.action == 'auto-fill'
+				) {
 					action = {
 						...rule.actions[index],
 						calculatorFields: []
