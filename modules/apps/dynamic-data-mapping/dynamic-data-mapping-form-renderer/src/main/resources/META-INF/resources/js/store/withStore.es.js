80c2ed7f31fab27426a42764b05adffb7d041655
Bruno Basto <bruno.basto@liferay.com>
2019-06-18 19:29:28 -0300

    WIP

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/store/withStore.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/store/withStore.es.js
@@ -36,9 +36,14 @@ const _handleFieldEdited = function(properties) {
 };
 
 const _handleFieldBlurred = function(properties) {
+	const {fieldInstance} = properties;
 	const {pages} = this;
 
 	handleFieldBlurred(pages, properties).then(blurredFieldPages => {
+		if (fieldInstance.isDisposed()) {
+			return;
+		}
+
 		this.setState({
 			pages: blurredFieldPages
 		});
