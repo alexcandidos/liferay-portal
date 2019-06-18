3ad7011a727888ae0430739a327e8ee6b647bee3
Bruno Basto <bruno.basto@liferay.com>
2019-06-18 00:56:28 -0300

    WIP

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/store/withStore.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/store/withStore.es.js
@@ -18,6 +18,10 @@ const _handleFieldEdited = function(properties) {
 	const evaluatorContext = this.getEvaluatorContext();
 
 	handleFieldEdited(evaluatorContext, properties).then(evaluatedPages => {
+		if (fieldInstance.isDisposed()) {
+			return;
+		}
+
 		this.setState(
 			{
 				pages: evaluatedPages
